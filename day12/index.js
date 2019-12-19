c099e0b27c6f3f00b63255efea2edaf1d0c49433
Shashi Shekhar <shashi20008@gmail.com>
2019-12-19 19:18:05 +0530

    day12: Executing using nodeJS

---
+++ day12/index.js
@@ -0,0 +1,15 @@
+const lodash = require('lodash');
+const express = require('express');
+
+console.log('Hello world on server!');
+
+let firstName = 'Shashi';
+const lastName = 'Shekhar';
+
+const obj = { 
+  firstName, 
+  lastName,
+  city: 'Chennai'
+};
+
+const coordinates = [ 10.4563, 123.456 ];
